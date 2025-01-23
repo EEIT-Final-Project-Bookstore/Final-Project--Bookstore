77d766d7848c96971dba3ca458e9d54c0546269f
Mick <mick20020607@gmail.com>
2025-01-23 14:36:53 +0800

    0123修正版第二版

---
+++ bookstore0123 ver2/src/main/java/bookstore/repository/CategoryRepository.java
@@ -0,0 +1,5 @@
+package bookstore.repository;
+
+public class CategoryRepository {
+
+}
---
+++ bookstore0123 ver2/src/main/java/bookstore/repository/CustomerRepository.java
@@ -0,0 +1,19 @@
+package bookstore.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import bookstore.domain.CustomerBean;
+
+
+public interface CustomerRepository extends JpaRepository<CustomerBean, Long> {
+
+	Optional<CustomerBean> findByUsername(String username);
+
+	boolean existsByUsername(String username);
+
+	void deleteByUsername(String username);
+
+	boolean existsByEmail(String email);
+}
