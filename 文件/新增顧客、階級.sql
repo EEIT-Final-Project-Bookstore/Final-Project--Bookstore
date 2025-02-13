e6c20c2f6ee72f285756ad27bf37276ee473ee89
Grooot-xx <brian970112@gmail.com>
2025-02-13 10:50:11 +0800

    Add files via upload

---
+++ 文件/新增顧客、階級.sql
@@ -0,0 +1,11 @@
+Insert into Admins values ('hyper','asd123','asd123@gamil.com',1);  
+ Insert into Admins values ('troll','asd456','asd456@gamil.com',2);  
+ Insert into Admins values ('while','asd789','asd789@gamil.com',3);
+
+ Insert into Ranks values ('頭頭');  
+ Insert into Ranks values ('大頭頭');  
+ Insert into Ranks values ('超大頭頭');
+
+insert into Customers values('黃先生','Coca Cola','abcd123','abcd123@gmail.com',785621436,912345678,GETDATE());
+insert into Customers values('張先生','Fofo','bcde123','bcde123@gmail.com',785645636,987654321,GETDATE());
+insert into Customers values('李小姐','Shampoo','cdef123','cdef123@gmail.com',225645636,976543210,GETDATE());
