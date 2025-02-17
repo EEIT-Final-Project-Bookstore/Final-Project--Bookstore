381961f956227fd11eba7f086e0abaa684146c18
Grooot-xx <brian970112@gmail.com>
2025-02-17 16:37:28 +0800

    Add files via upload

--- 文件/新增顧客、階級.sql
+++ 文件/新增顧客、階級.sql
@@ -1,11 +1,12 @@
-Insert into Admins values ('hyper','asd123','asd123@gamil.com',1);  
- Insert into Admins values ('troll','asd456','asd456@gamil.com',2);  
- Insert into Admins values ('while','asd789','asd789@gamil.com',3);
+Insert into Admins values ('andy','asd123','asd123@gamil.com',1);  
+Insert into Admins values ('bella','asd456','asd456@gamil.com',2);  
+Insert into Admins values ('cindy','asd789','asd789@gamil.com',3);
 
- Insert into Ranks values ('頭頭');  
- Insert into Ranks values ('大頭頭');  
- Insert into Ranks values ('超大頭頭');
+Insert into Ranks values ('經理');  
+Insert into Ranks values ('主管');  
+Insert into Ranks values ('組長');
+Insert into Ranks values ('員工');
 
-insert into Customers values('黃先生','Coca Cola','abcd123','abcd123@gmail.com',785621436,912345678,GETDATE());
-insert into Customers values('張先生','Fofo','bcde123','bcde123@gmail.com',785645636,987654321,GETDATE());
-insert into Customers values('李小姐','Shampoo','cdef123','cdef123@gmail.com',225645636,976543210,GETDATE());
+insert into Customers values('黃先生','alan6a','aaaaaa','alan6a@gmail.com',0785621436,0912345678,GETDATE());
+insert into Customers values('張先生','bowen6b','bbbbbb','bowen6b@gmail.com',0785645636,0987654321,GETDATE());
+insert into Customers values('李小姐','candy6c','cccccc','candy6c@gmail.com',0225645636,0976543210,GETDATE());
